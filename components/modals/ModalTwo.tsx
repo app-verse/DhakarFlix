8132fe1b8514045ebb114d07778359f03ccb808d
app-verse <tokomnyori3@gmail.com>
2023-08-27 04:12:13 +0530

    edited

--- components/modals/ModalTwo.tsx
+++ components/modals/ModalTwo.tsx
@@ -1,5 +1,9 @@
 import { FaInstagram } from 'react-icons/fa'
-import { AiFillGithub } from 'react-icons/ai'
+import { BiMoviePlay } from 'react-icons/bi'
+import { VscHistory } from 'react-icons/vsc'
+import { MdTravelExplore } from 'react-icons/md'
+import { GiAmpleDress } from 'react-icons/gi'
+import { FaBowlFood } from 'react-icons/fa6'
 import { useRef, useEffect } from 'react'
 
 interface ModalTwoProps {
@@ -40,18 +44,26 @@ const ModalTwo: React.FC<ModalTwoProps> = ({ greet, greetings }) => {
                     <div className='modal-body'>
                         <div className='details-two'>
                             <div className='movie-description text-left'>
-                                <h1 className='text-red-400 font-bold'>I am Tokom,</h1>
-                                <p>a mobile and web app developer with a passion for nutrition science and economics.</p>
+                                <h1 className='text-red-400 font-bold'>I am Geyi,</h1>
+                                <p>a culinary historian, fashionista, cinephile, and traveler.
+                                    <span>
+                                        <BiMoviePlay className='inline text-red-400 text-lg ml-1 mr-1' />
+                                        <FaBowlFood className='inline text-slate-400 text-md mr-1' />
+                                        <MdTravelExplore className='inline text-blue-400 text-xl mr-1' />
+                                        <VscHistory className='inline text-green-400' />
+                                        <GiAmpleDress className='inline text-pink-400 text-lg mr-1' />
+                                    </span>
+                                </p>
                             </div>
                             <div className='modal-image-cover mb-2'>
                                 <img
                                     className='modal-image-two h-full'
-                                    src="/images/Mee.jpg" alt=""
+                                    src="/images/geyii2.jpg" alt=""
                                 />
                                 <a
                                     className='insta-link text-md flex justify-end items-center gap-1 cursor-pointer mr-1'
                                     target='_blank'
-                                    href="https://www.instagram.com/geekpie.in/">
+                                    href="https://www.instagram.com/meformusic8/">
                                     <FaInstagram className='text-pink-600 text-md shadow-lg font-bold' />
                                     <span className='text-sm'>Follow</span>
                                 </a>
@@ -59,14 +71,11 @@ const ModalTwo: React.FC<ModalTwoProps> = ({ greet, greetings }) => {
                         </div>
                         <div className='bottom-part text-left mb-0 mt-1'>
                             <p>
-                                <span className='font-bold text-green-400'>Nextify is a web app </span>
-                                that provides users with information and trailer videos about
-                                Movies, Anime, and TV series. The app is built using Next.js, TypeScript, and Tailwind CSS,
-                                leveraging the TMDB API and Jikan REST API.
-                                <a href="https://github.com/TokomNyori/NextDB" target='_blank'>
-                                    &nbsp; <AiFillGithub className='text-white text-lg shadow-lg inline font-bold' />
-                                    <span className='text-sm'> Github Repo</span>
-                                </a>
+                                <span className='font-bold text-green-400'>This web app </span>
+                                provides users with information and trailer videos about
+                                Movies, Anime, and TV series. This web app is a thoughtful gift from my good friend,
+                                who is a software developer. The app is built using Next.js, TypeScript, and
+                                Tailwind CSS, leveraging the TMDB API and Jikan REST API.
                             </p>
                             <div className='mb-0 mt-3'>
                                 <img
